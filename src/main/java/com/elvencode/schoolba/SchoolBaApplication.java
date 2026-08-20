4a1960ddc094b7ea39b2a868dacfbeb92d8b9f35
pathum <pathum.developer@gmail.com>
2026-08-20 11:29:41 +0530

    Initial commit: Spring Boot project scaffold

---
+++ src/main/java/com/elvencode/schoolba/SchoolBaApplication.java
@@ -0,0 +1,13 @@
+package com.elvencode.schoolba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elvencode/schoolba/SchoolBaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvencode.schoolba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
