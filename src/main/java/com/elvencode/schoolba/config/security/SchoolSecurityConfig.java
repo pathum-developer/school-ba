853ec8e8f329627ae308aca1b9194a3bcd89d5a5
pathum <pathum.developer@gmail.com>
2026-08-30 19:15:36 +0530

    110 - Add defaultSecurityFilterChain to enable form and basic authentication

---
+++ src/main/java/com/elvencode/schoolba/config/security/SchoolSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.elvencode.schoolba.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SchoolSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
