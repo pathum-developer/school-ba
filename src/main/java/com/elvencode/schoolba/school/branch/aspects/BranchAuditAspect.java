a3af419cfada4646aa14833787baf5e19fb2ddab
pathum <pathum.developer@gmail.com>
2026-08-31 18:45:50 +0530

    14-153 - Add after-returning aspect to log deleted branch

--- src/main/java/com/elvencode/schoolba/school/branch/aspects/BranchAuditAspect.java
+++ src/main/java/com/elvencode/schoolba/school/branch/aspects/BranchAuditAspect.java
@@ -2,6 +2,7 @@ package com.elvencode.schoolba.school.branch.aspects;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,17 @@ public class BranchAuditAspect {
     public void auditBranchChange(JoinPoint joinPoint) {
         log.info(
                 "Branch change requested: method={}, args={}",
+                joinPoint.getSignature().toShortString(),
+                Arrays.toString(joinPoint.getArgs())
+        );
+    }
+
+    @AfterReturning("""
+            execution(* com.elvencode.schoolba.school.branch.service.impl.BranchServiceImpl.deleteBranchByCode(..))
+            """)
+    public void auditSuccessfulBranchDelete(JoinPoint joinPoint) {
+        log.info(
+                "Branch delete completed: method={}, args={}",
                 joinPoint.getSignature().toShortString(),
                 Arrays.toString(joinPoint.getArgs())
         );
