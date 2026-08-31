b4fa6c594e70308e6567fdbf818969c9a0f81673
pathum <pathum.developer@gmail.com>
2026-08-31 23:05:27 +0530

    15-160 - Get branch details using JPQL queries

--- src/main/java/com/elvencode/schoolba/school/branch/repository/BranchRepository.java
+++ src/main/java/com/elvencode/schoolba/school/branch/repository/BranchRepository.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 import com.elvencode.schoolba.school.branch.entity.Branch;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,8 +18,18 @@ public interface BranchRepository extends JpaRepository<Branch, UUID> {
 
     Optional<Branch> findBySchool_IdAndCode(UUID schoolId, String code);
 
-    @EntityGraph(attributePaths = {"school", "contactNoList"})
-    Optional<Branch> findDetailedBySchool_IdAndCode(UUID schoolId, String code);
+    @Query("""
+            select distinct branch
+            from Branch branch
+            join fetch branch.school school
+            left join fetch branch.contactNoList
+            where school.id = :schoolId
+                    and branch.code = :code
+            """)
+    Optional<Branch> findDetailedBySchool_IdAndCode(
+            @Param("schoolId") UUID schoolId,
+            @Param("code") String code
+    );
 
     boolean existsBySchool_IdAndCode(UUID schoolId, String code);
 
